66fbbc0707143de4156d50f68d8a7f4c8aad104c
Tuntai <aparajitapaul98@gmail.com>
2021-02-12 19:20:19 +0530

    Changes in main.html and views.py [look enhancement]

--- assistant-dashboard-main/dashboard/views.py
+++ assistant-dashboard-main/dashboard/views.py
@@ -7,6 +7,119 @@ from urllib.request import urlopen
 from lxml import etree
 import threading, time
 from threading import Thread
+import smtplib
+import time
+import imaplib
+import email
+import traceback 
+import pprint 
+import json
+
+
+def index(request):
+
+    mail_info = {}
+    mail_info = read_email_from_gmail()
+
+    fin_news = {}
+    fin_news  = news()
+
+    print(fin_news)
+
+    
+    return render(request, 'dashboard/main.html', {'mail_info': mail_info, 'fin_news': fin_news})
+
+# -------------------------------------------------
+#
+# Utility to read email from Gmail Using Python
+#
+# ------------------------------------------------
+
+FROM_EMAIL = "teastmaildjapp@gmail.com" 
+FROM_PWD = "Testpass123" 
+SMTP_SERVER = "imap.gmail.com" 
+SMTP_PORT = 993
+
+def read_email_from_gmail():
+    try:
+        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+        mail.login(FROM_EMAIL,FROM_PWD)
+        mail.select('Inbox')
+
+        data = mail.search(None, 'ALL')
+        mail_ids = data[1]
+        id_list = mail_ids[0].split()   
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
+
+        count = 0
+        fin = {}
+        for i in range(latest_email_id,first_email_id, -1):
+            count += 1
+            if(count == 10):
+                break;
+            data = mail.fetch(str(i), '(RFC822)' )
+            for response_part in data:
+                arr = response_part[0]
+                if isinstance(arr, tuple):
+                    msg = email.message_from_string(str(arr[1],'utf-8'))
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    email_time = msg['time']
+                    print("////////////////")
+                    print(email_time)
+                    print("/////////////////")
+                                        
+                    fin[email_from] = email_subject
+        print(fin)
+        return fin
+
+    except Exception as e:
+        traceback.print_exc() 
+        print(str(e))
+
+
+def news():
+      
+    secret = "6d1a6117f1d24ecaaa2f054c7e8b58a6"
+       
+    # Define the endpoint 
+    url = 'https://newsapi.org/v2/everything?'
+       
+    # Specify the query and 
+    # number of returns 
+    parameters = { 
+        'q': 'merkel', # query phrase 
+        'pageSize': 100,  # maximum is 100 
+        'apiKey': secret # your own API key 
+    } 
+       
+    # Make the request 
+    response = requests.get(url,  params = parameters) 
+       
+    # Convert the response to  
+    # JSON format and pretty print it 
+    response_json = response.json() 
+    news_data = {}
+
+    news_data = response_json
+
+    fin_data = {}
+    count = 0
+
+    for things in news_data["articles"]:
+        place = (things["source"]["name"])
+        thenews = (things["title"])
+        count += 1
+
+        fin_data[place] = thenews
+
+        if count == 10 :
+            break
+
+    print(fin_data)
+
+    return fin_data
 
 user_req = ""
 me = {}
@@ -20,41 +133,13 @@ class MyThread(Thread):
 
     def run(self):
         while True:
-            print(" I am the boss ")
+            print(" Working ")
             time.sleep(self.tstep)
             self.hfunc()
             # threading.Timer(self.tstep, self.hfunc, self.args).start()
 
-def index(request):
-    mail_info = {}
-
-    mail_info = read_email_from_gmail()
-
-    # print(mail_info)
-
-    name = []
-    msg = []
-
-    for sender, subject in mail_info.items():
-        name.append(sender)
-        msg.append(subject)
-
-    print(name, msg)
-
-    fin_news = {}
-
-    fin_news  = news()
-
-    print(fin_news)
-
-    
-    return render(request, 'dashboard/main.html', {'mail_info': mail_info, 'fin_news': fin_news})
-    # else:
-    #     return HttpResponse('/thanks/')
-#    return HttpResponse('hello')
-
 def track_value():
-    print("im being called")
+    print("I'm being called")
     global me
 
     def notify():
@@ -89,120 +174,3 @@ def output(request):
     print(data)
 
     return render(request, 'try.html', {'data': data})
-
-
-import smtplib
-import time
-import imaplib
-import email
-import traceback 
-# -------------------------------------------------
-#
-# Utility to read email from Gmail Using Python
-#
-# ------------------------------------------------
-ORG_EMAIL = "@gmail.com" 
-FROM_EMAIL = "teastmaildjapp@gmail.com" #+ ORG_EMAIL 
-FROM_PWD = "Testpass123" 
-SMTP_SERVER = "imap.gmail.com" 
-SMTP_PORT = 993
-
-def read_email_from_gmail():
-    try:
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(FROM_EMAIL,FROM_PWD)
-        mail.select('Inbox')
-
-        data = mail.search(None, 'ALL')
-        mail_ids = data[1]
-        id_list = mail_ids[0].split()   
-        first_email_id = int(id_list[0])
-        latest_email_id = int(id_list[-1])
-
-        count = 0
-        fin = {}
-        for i in range(latest_email_id,first_email_id, -1):
-            count += 1
-            # if(count == 8):
-            #     break;
-            data = mail.fetch(str(i), '(RFC822)' )
-            for response_part in data:
-                arr = response_part[0]
-                if isinstance(arr, tuple):
-                    msg = email.message_from_string(str(arr[1],'utf-8'))
-                    email_subject = msg['subject']
-
-                    # text = ''.join((c for c in str(email_subject) if ord(c) < 128))
-
-                    # email_subject = text
-
-                    email_from = msg['from']
-                    email_body = msg['body']
-                    
-                    fin[email_from] = email_subject
-        print(fin)
-        return fin
-
-                    # print('From : ' + email_from + '\n')
-                    # print('Subject : ' + email_subject + '\n')
-
-    except Exception as e:
-        traceback.print_exc() 
-        print(str(e))
-
-
-
-import pprint 
-import json
-import requests 
-
-
-def news():
-      
-    secret = "6d1a6117f1d24ecaaa2f054c7e8b58a6"
-       
-    # Define the endpoint 
-    url = 'https://newsapi.org/v2/everything?'
-       
-    # Specify the query and 
-    # number of returns 
-    parameters = { 
-        'q': 'merkel', # query phrase 
-        'pageSize': 100,  # maximum is 100 
-        'apiKey': secret # your own API key 
-    } 
-       
-    # Make the request 
-    response = requests.get(url,  
-                            params = parameters) 
-       
-    # Convert the response to  
-    # JSON format and pretty print it 
-    response_json = response.json() 
-    # pprint.pprint(response_json) 
-    # with open('news1.json', 'w') as f:
-    news_data = {}
-
-    news_data = response_json
-    # json.dump(response_json, news_data, ensure_ascii=False, indent=4)
-
-    # nlist = []
-    # slist = []
-
-    fin_data = {}
-    count = 0
-
-    for things in news_data["articles"]:
-        place = (things["source"]["name"])
-        thenews = (things["title"])
-        count += 1
-
-        fin_data[place] = thenews
-
-        if count == 10 :
-            break
-
-    print(fin_data)
-    # print(len(nlist), len(slist))
-
-    return fin_data
